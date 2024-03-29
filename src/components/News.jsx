347420e9aaaf3bc47fe29faa66c3aa4a0c79a56d
Himanshu Gupta <guptasitapur489@gmail.com>
2024-03-30 04:17:44 +0530

    News fetch is working fine

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,42 +1,26 @@
 import React from "react";
 import NewsItem from "./NewsItem";
-
+import {useState, useEffect} from 'react'
 function News() {
-  const data = [
-    {
-      imgUrl: "https://example.com/image1.jpg",
-      title: "Sample News Title 1",
-      description: "This is a sample news description for the first news item.",
-      detailsUrl: "/news/sample-news-1",
-    },
-    {
-      imgUrl: "https://example.com/image2.jpg",
-      title: "Sample News Title 2",
-      description:
-        "This is a sample news description for the second news item.",
-      detailsUrl: "/news/sample-news-2",
-    },
-    {
-      imgUrl: "https://example.com/image3.jpg",
-      title: "Sample News Title 3",
-      description: "This is a sample news description for the third news item.",
-      detailsUrl: "/news/sample-news-3",
-    },
-    {
-      imgUrl: "https://example.com/image4.jpg",
-      title: "Sample News Title 4",
-      description:
-        "This is a sample news description for the fourth news item.",
-      detailsUrl: "/news/sample-news-4",
-    },
-    {
-      imgUrl: "https://example.com/image5.jpg",
-      title: "Sample News Title 5",
-      description: "This is a sample news description for the fifth news item.",
-      detailsUrl: "/news/sample-news-5",
-    },
-  ];
-
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+          const res = await fetch(
+            "https://newsapi.org/v2/top-headlines?country=us&apiKey=6d72f93a215346a6875c195f5709910f"
+          );
+          if(!res.ok){
+            throw new Error("Something went wrong")
+          }
+          const jsonData = await res.json();
+          // console.log(jsonData.articles[0]);
+          setData(jsonData.articles);
+      }catch(error){
+          console.error("Error fetching data: ", error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div className="container d-flex flex-row gap-4 flex-wrap mt-2 mb-2 p-2">
       {data.map((element, index) => (
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,13 +6,28 @@ function NewsItem(props) {
     width: "18rem",
   };
 
+  const defaultImage = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2024-03/240329-francis-scott-key-bridge-al-0855-d9319e.jpg";
+
+  // Check if all required properties are available
+  if (!props.title || !props.description || !props.url || !props.urlToImage) {
+    return null; 
+  }
   return (
     <div className="card" style={cardStyle}>
-      <img src={props.imgUrl} className="card-img-top" alt="..." />
+      <img
+        src={props.urlToImage ? props.urlToImage : defaultImage}
+        className="card-img-top"
+        alt="..."
+      />
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.description}</p>
-        <Link to={props.detailsUrl} className="btn btn-primary">
+        <h5 className="card-title">{props.title.slice(0, 40)}...</h5>
+        <p className="card-text">{props.description.slice(0, 100)}...</p>
+        <Link
+          to={props.url}
+          className="btn btn-primary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Details
         </Link>
       </div>
